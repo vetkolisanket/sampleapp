e1a2b6401c6d5aad52ed8a049fc9dbf5b450f91c
Sanket <vetkolisanket@gmail.com>
2019-05-11 09:54:17 +0530

    Created 3 variants of the app by separating it out into 3 packages based on build variant

--- app/src/staging/java/Injection.kt
+++ app/src/staging/java/Injection.kt
@@ -1,7 +1,13 @@
+import com.sanket.sampleapp.features.onboarding.contracts.ILoginContract
+import com.sanket.sampleapp.features.onboarding.presenters.LoginPresenter
+
 /**
  * Created by Sanket on 03/05/19.
  */
 object Injection {
+    fun getLoginPresenter(): ILoginContract.Presenter {
+        return LoginPresenter()
+    }
 
     const val PREFERENCE_FILE_NAME = "SAMPLE_APP_STAGING"
 
