eed0c17015450e32475b15644796357ee24d2e1b
Sanket <vetkolisanket@gmail.com>
2019-05-12 23:24:58 +0530

    Added empty test class for SA

---
+++ app/src/androidTest/java/com/sanket/sampleapp/features/onboarding/ui/activities/SignUpActivityTest.kt
@@ -0,0 +1,11 @@
+package com.sanket.sampleapp.features.onboarding.ui.activities
+
+import androidx.test.runner.AndroidJUnit4
+import org.junit.runner.RunWith
+
+/**
+ * Created by Sanket on 12/05/19.
+ */
+@RunWith(AndroidJUnit4::class)
+class SignUpActivityTest {
+}
