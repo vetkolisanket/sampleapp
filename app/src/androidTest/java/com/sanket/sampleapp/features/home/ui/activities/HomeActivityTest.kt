4dd58d0e92059c267aead58e4059dcb71b84eec7
Sanket <vetkolisanket@gmail.com>
2019-05-19 23:00:37 +0530

    Added empty instrumentation test class for HA

---
+++ app/src/androidTest/java/com/sanket/sampleapp/features/home/ui/activities/HomeActivityTest.kt
@@ -0,0 +1,10 @@
+package com.sanket.sampleapp.features.home.ui.activities
+
+
+import com.sanket.sampleapp.base.BaseInstrumentationTest
+
+/**
+ * Created by Sanket on 19/05/19.
+ */
+class HomeActivityTest: BaseInstrumentationTest() {
+}
